21bcfa9caa2dcdc7c578fdb07ca570a2ff436a42
Roman Shevchuk <rommssh@gmail.com>
2016-01-09 21:02:26 +0200

    Added classpath

--- scripts/run_webservice_server.sh
+++ scripts/run_webservice_server.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-java -cp ../target/dblab.jar me.dblab.server.DatabaseWebService 7777
+#java -cp ../target/dblab.jar me.dblab.server.DatabaseWebService --port=7777
+java -classpath /home/user/labs/DatabaseProject/target/classes:/home/user/labs/DatabaseProject/target/repo/commons-cli/commons-cli/1.3.1/commons-cli-1.3.1.jar me.dblab.server.DatabaseWebService --port=7000 --database=../sample.db
